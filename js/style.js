3c7433ddd9eacfd80dbfc187abefa3c5975a70bb
Nnookk <naokiogata@NaokinoAir.com>
2022-02-03 15:19:47 -0500

    first commit

---
+++ js/style.js
@@ -0,0 +1,60 @@
+
+$('#bBtn').on('click',function(){
+    if ($('#breadInp').val() >= 1) {
+        $('.breadPop')
+            .animate({opacity: '1'},500)
+            .fadeOut(6000);
+    }
+})
+
+$('#cBtn').on('click',function(){
+    if ($('#cookieInp').val() >= 1) {
+        $('.cookiePop')
+            .animate({opacity: '1'},500)
+            .fadeOut(6000);
+    }
+})
+
+$('#dobtn').on('click',function(){
+    if ($('#doughnutInp').val() >= 1) {
+        $('.doughnutPop')
+            .animate({opacity: '1'},500)
+            .fadeOut(6000);
+    }
+})
+
+$('#cakeBtn').on('click',function(){
+    if ($('#cakeInp').val() >= 1) {
+        $('.cakePop')
+            .animate({opacity: '1'},500)
+            .fadeOut(6000);
+    }
+})
+
+$('#mufBtn').on('click',function(){
+    if ($('#muffinInp').val() >= 1) {
+        $('.muffinPop')
+            .animate({opacity: '1'},500)
+            .fadeOut(6000);
+    }
+})
+
+$('#cherryBtn').on('click',function(){
+    if ($('#cherryInp').val() >= 1) {
+        $('.cherryPop')
+            .animate({opacity: '1'},500)
+            .fadeOut(6000);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
